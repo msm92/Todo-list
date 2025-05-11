483d651a1d68439b4da389ece756e1d59077925e
msm92 <mahdi.sadeghi.m@gmail.com>
2025-05-11 20:14:58 +0330

    add react and bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+  return(
+     <div className="container mt-5">
+       <h1 className="text-body">تو دو لیست خفن</h1>
+       <button className="btn btn-primary">تست</button>
+     </div>
+  );
+}
+
+export default App;
