039c73f7f2974d0a3cef009b2f83112b83c6459f
msm92 <mahdi.sadeghi.m@gmail.com>
2025-05-20 21:44:52 +0330

    add edti and delete func to TaskController

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -10,17 +10,54 @@ exports.getTasks = async (req, res) => {
     }
 };
 
+exports.getTask = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        res.json({ code: 1, msg: "عملیات با موفقیت انجام شد." });
+    } catch (error) {
+        res.json({ code: 0, msg: error.message });
+    }
+};
+
 exports.createTask = async (req, res) => {
     const task = new Task({
         title: req.body.title,
         description: req.body.description,
         dueDate: req.body.dueDate,
-        user: mongoose.ObjectId("654874s656a78643a"),
+        user: req.body.user,
     });
     try {
         const newTask = await task.save();
         res.status(201).json({ code: 1, message: "ok", newTask });
     } catch (err) {
         res.status(400).json({ code: 0, message: err.message });
+    }
+};
+
+exports.edit = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) {
+            return res
+                .status(404)
+                .json({ code: 0, msg: "خطا در انجام عملیات" });
+        }
+        res.json({ code: 1, task });
+    } catch (err) {
+        res.status(500).json({ code: 0, message: err.message });
+    }
+};
+
+exports.delete = async (req, res) => {
+    let task = await Task.findById(req.params.id);
+    if (!task) {
+        return res.json({ code: 0, msg: "خطا در انجام عملیات" });
+    }
+
+    try {
+        await Task.findByIdAndDelete(req.params.id);
+        res.json({ code: 0, msg: "عملیات با موفقیت انجام شد." });
+    } catch (error) {
+        res.status(500).json({ code: 0, msg: error.message });
     }
 };
